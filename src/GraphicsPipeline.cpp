8820f36be9b014696835bbdf0c8073ef2a59f724
Sławek <slawekzdunek2.3.6@gmail.com>
2026-09-24 17:59:43 +0200

    Constness addition

--- src/GraphicsPipeline.cpp
+++ src/GraphicsPipeline.cpp
@@ -6,7 +6,7 @@
 
 namespace vg {
 GraphicsPipeline::GraphicsPipeline(
-    uint32_t pipelineLayout, const std::vector<Shader *> &shaders, const VertexLayout &vertexInput,
+    uint32_t pipelineLayout, const std::vector<const Shader *> &shaders, const VertexLayout &vertexInput,
     const InputAssembly &inputAssembly, const Tesselation &tesselation, const ViewportState &viewportState,
     const Rasterizer &rasterizer, const Multisampling &multisampling, const DepthStencil &depthStencil,
     const ColorBlending &colorBlending, const std::vector<DynamicState> &dynamicState, int parentIndex,
--- src/GraphicsPipeline.h
+++ src/GraphicsPipeline.h
@@ -10,7 +10,7 @@ class GraphicsPipeline {
 
   public:
     GraphicsPipeline(
-        uint32_t pipelineLayout, const std::vector<Shader *> &shaders, const VertexLayout &vertexInput,
+        uint32_t pipelineLayout, const std::vector<const Shader *> &shaders, const VertexLayout &vertexInput,
         const InputAssembly &inputAssembly, const Tesselation &tesselation, const ViewportState &viewportState,
         const Rasterizer &rasterizer, const Multisampling &multisampling, const DepthStencil &depthStencil,
         const ColorBlending &colorBlending, const std::vector<DynamicState> &dynamicState, int parentIndex = -1,
@@ -25,7 +25,7 @@ class GraphicsPipeline {
     GraphicsPipeline &operator=(GraphicsPipeline &&) = default;
 
     uint32_t pipelineLayout;
-    std::vector<Shader *> shaders;
+    std::vector<const Shader *> shaders;
     VertexLayout vertexInput;
     InputAssembly inputAssembly;
     Tesselation tesselation;
